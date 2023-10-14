3e6fdd4e8542ac88135b076b88ff720e022e5887
namnn.17b3 <=>
2023-10-14 19:28:26 +0700

    api authen and face id(not complete)

---
+++ backend-iot/authen/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+app_name = 'authen'
+
+urlpatterns = [
+    path('test-socket', view=views.index, name='index'),
+    path('login', view=views.Login.as_view(), name='login'),
+    path('me', view=views.Me.as_view(), name='me'),
+    path('refresh-token', view=views.RefreshToken.as_view(), name='refresh-token'),
+    path('register', view=views.Register.as_view(), name='register'),
+    path('missing-password', view=views.MissingPassword.as_view(), name='missing-password'),
+    path('logout', view=views.Logout.as_view(), name='logout'),
+]
