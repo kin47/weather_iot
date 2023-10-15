673527347210ee1a737c679195b49b2f100fc3e9
namnn.17b3 <=>
2023-10-15 12:51:29 +0700

    font end call api logout

--- frontend-iot/js/home.js
+++ frontend-iot/js/home.js
@@ -6,7 +6,22 @@ callAPI('api/authen/me', 'GET', null, function() {
             document.querySelector('#email span').innerText = data['email'];
         }
         else if (this.status == 401) {
-            redirect('./401_unauthorized.html');
+            redirect('./error/401_unauthorized.html');
         }
     }
 });
+
+document.querySelector('#logout-btn').onclick = () => {
+    callAPI('api/authen/logout', 'GET', null, function() {
+        if (this.readyState === 4) {
+            data = JSON.parse(this.responseText);
+            if (this.status == 200) {
+                localStorage.removeItem('accessToken');
+                redirect('./authen/login.html');
+            }
+            else if (this.status == 400) {
+                redirect('./error/401_unauthorized.html');
+            }
+        }
+    });
+}
